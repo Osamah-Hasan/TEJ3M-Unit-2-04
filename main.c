f56145993cda515e233aceacaaeab018aeb3f289
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2025-03-04 09:10:39 -0500

    changed pin mode changed numbers for time add const add blink time deleted unused comment

--- main.c
+++ main.c
@@ -3,31 +3,54 @@ Created by: Osamah
 Created on: Feb 2025
 RGB projrct on the breadBoard
 */
-/*
-Created by: Osamah
-Created on: Feb 2025
-RGB projrct on the breadBoard
-*/
+
+const int PIN_11 = 11;
+const int PIN_12 = 12;
+const int PIN_13 = 13;
+int blinkTime = 1000;
 
 void setup()
 {
-  pinMode (11, OUTPUT);
-  pinMode (12, OUTPUT);
-  pinMode(13, OUTPUT);
+  pinMode (PIN_11, OUTPUT);
+  pinMode (PIN_12, OUTPUT);
+  pinMode(PIN_13, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);
-  digitalWrite(12, LOW);
-  digitalWrite(11, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(13, LOW);
-  digitalWrite(12, HIGH);
-  digitalWrite(11, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(13, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(11, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  //turn on red only
+  digitalWrite(PIN_13, HIGH);
+  digitalWrite(PIN_12, LOW);
+  digitalWrite(PIN_11, LOW);
+  delay(blinkTime); // Wait for 1000 millisecond(s)
+  //turn on green only
+  digitalWrite(PIN_13, LOW);
+  digitalWrite(PIN_12, HIGH);
+  digitalWrite(PIN_11, LOW);
+  delay(blinkTime); // Wait for 1000 millisecond(s)
+  //turn on blue only
+  digitalWrite(PIN_13, LOW);
+  digitalWrite(PIN_12, LOW);
+  digitalWrite(PIN_11, HIGH);
+  delay(blinkTime); // Wait for 1000 millisecond(s)
+  //turn on red and green
+  digitalWrite(PIN_13, HIGH);
+  digitalWrite(PIN_12, HIGH);
+  digitalWrite(PIN_11, LOW);
+  delay(blinkTime); // Wait for 1000 millisecond(s)
+  //turn on green and blue
+  digitalWrite(PIN_13, LOW);
+  digitalWrite(PIN_12, HIGH);
+  digitalWrite(PIN_11, HIGH);
+  delay(blinkTime); // Wait for 1000 millisecond(s)
+  //turn on red and blue
+  digitalWrite(PIN_13, HIGH);
+  digitalWrite(PIN_12, LOW);
+  digitalWrite(PIN_11, HIGH);
+  delay(blinkTime); // Wait for 1000 millisecond(s)
+  //turn all on
+  digitalWrite(PIN_13, HIGH);
+  digitalWrite(PIN_12, HIGH);
+  digitalWrite(PIN_11, HIGH);
+  delay(blinkTime); // Wait for 1000 millisecond(s)
 }
